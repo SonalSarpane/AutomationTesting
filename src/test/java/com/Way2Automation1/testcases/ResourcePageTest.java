8bffa91e87c4fe8d6e1944b38c24d22fac9bdab0
Mahesh Londhe <mahesh@gulvel.com>
2020-03-22 22:04:41 +0530

    Files added

---
+++ src/test/java/com/Way2Automation1/testcases/ResourcePageTest.java
@@ -0,0 +1,34 @@
+package com.Way2Automation1.testcases;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import com.Way2Automation1.base.TestBase;
+import com.Way2Automation1.pages.ResourcePage;
+
+public class ResourcePageTest extends TestBase {
+	
+	ResourcePage resourcepage;
+	
+	public ResourcePageTest()
+	{
+		super();
+	}
+	
+	@BeforeMethod
+	public void setup()
+	{
+		Initialization();
+		resourcepage=new ResourcePage();
+	}
+
+	@Test(priority=1)
+	public void validateResourceTab() throws Throwable
+	{
+		resourcepage.ClickOnResource();
+	}
+    
+	/*@AfterMethod
+	public void tearDown()
+	{
+		driver.quit();
+	}*/
+}
